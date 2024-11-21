00a4f876e6950586c447c78941612dfabcfa4706
Nabilfadilah <mohammadfadilah00@gmail.com>
2024-11-21 11:49:47 +0700

    Menambah section untuk bungkus app page

--- anime/src/app/page.js
+++ anime/src/app/page.js
@@ -1,35 +1,25 @@
 import AnimeList from "@/components/AnimeList"
-import Typography from "../elements/button/text/Typography";
-import Link from "next/link";
+import Header from "@/components/AnimeList/Header";
 
-const Home = async () => {
+const Page = async () => {
 
   {/* server component, gak bisa pake useEffect, useState, dll.. */ }
   const response = await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/top/anime?limit=8`)
 
   // keluarkan dulu datanya
-  const anime = await response.json()
+  const topAnime = await response.json()
   // console.log('Ini data api anime : ', anime);
 
   return (
-    // <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-    <div>
-      <div className="flex justify-between items-center p-4">
-        <Typography className="text-2xl font-bold">Paling Popular</Typography>
-        <Link href="/populer" className="md:text-lg text-md underline hover:text-indigo-500 transition-all">Lihat Semua</Link>
-      </div>
-
-      <div className="grid md:grid-cols-4 sm:grid-cols-3 grid-cols-2 gap-4 px-4">
-        {anime.data.map(data => {
-          return (
-            <div key={data.mal_id} className="shadow-xl">
-              <AnimeList id={data.mal_id} title={data.title} images={data.images.webp.image_url} />
-            </div>
-          )
-        })}
-      </div>
-    </div>
+    <>
+      {/* anime populer */}
+      <section>
+        {/* anime list punya data api dari topAnime */}
+        <Header title="Paling Populer" linkTitle="Lihat Semua" linkHref="/populer" />
+        <AnimeList api={topAnime} />
+      </section>
+    </>
   );
 }
 
-export default Home;
+export default Page;
