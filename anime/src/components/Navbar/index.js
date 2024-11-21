c494041fe88e532c2650f7ca2a7ece8aadf4ba6b
Nabilfadilah <mohammadfadilah00@gmail.com>
2024-11-21 17:06:10 +0700

    Merubah bg warna navbar

--- anime/src/components/Navbar/index.js
+++ anime/src/components/Navbar/index.js
@@ -4,9 +4,9 @@ import React from 'react'
 
 const Navbar = () => {
     return (
-        <header className='bg-lime-900'>
-            <div className='flex md:flex-row flex-col justify-between p-4 gap-2'>
-                <Link href="/" className='font-bold text-white text-2xl' >ANIMEGS</Link>
+        <header className='bg-color-accent'>
+            <div className='flex md:flex-row flex-col justify-between md:items-center p-4 gap-2'>
+                <Link href="/" className='font-bold text-white text-2xl'>ANIMEGS</Link>
                 <InputSeacrh placeholder="Cari anime..." />
             </div>
         </header>
