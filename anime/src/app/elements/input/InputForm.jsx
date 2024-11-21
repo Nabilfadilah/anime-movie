eab90af8d2fdcbfdce9786c5cac9e60a0c03461a
Nabilfadilah <mohammadfadilah00@gmail.com>
2024-11-21 07:10:33 +0700

    Membuat element InputForm

---
+++ anime/src/app/elements/input/InputForm.jsx
@@ -0,0 +1,41 @@
+import { forwardRef } from "react";
+import Label from "./Label";
+import Input from "./Input";
+
+const InputForm = forwardRef((props, ref) => {
+  const {
+    label,
+    name,
+    type,
+    placeholder,
+    className,
+    onChange,
+    value,
+    accept,
+    onBlur,
+    error,
+    helperText,
+  } = props; // Ambil className dari props
+
+  return (
+    <div className="mb-3">
+      <Label htmlFor={name}>{label}</Label>
+      <Input
+        name={name}
+        type={type}
+        placeholder={placeholder}
+        ref={ref}
+        className={className} // Gunakan className yang diterima dari props
+        onChange={onChange}
+        // required
+        value={value}
+        accept={accept}
+        onBlur={onBlur}
+        error={error}
+        helperText={helperText}
+      />
+    </div>
+  );
+});
+
+export default InputForm;
