959e14285da9397e3ad141e4e3d1b97bc7f49571
Nabilfadilah <mohammadfadilah00@gmail.com>
2024-11-21 17:05:44 +0700

    Merubah warna header animeList

--- anime/src/components/AnimeList/Header.jsx
+++ anime/src/components/AnimeList/Header.jsx
@@ -1,18 +1,25 @@
-import React from 'react'
-import Typography from '@/elements/button/text/Typography'
-import Link from 'next/link'
+import React from "react";
+import Typography from "@/elements/button/text/Typography";
+import Link from "next/link";
 
-const Header = ({title, linkHref, linkTitle}) => {
+const Header = ({ title, linkHref, linkTitle }) => {
   return (
     <div>
-        <div className="flex justify-between items-center p-4">
-            <Typography className="text-2xl font-bold">{title}</Typography>
-            {linkHref && linkTitle ? 
-                <Link href={linkHref} className="md:text-lg text-md underline hover:text-indigo-500 transition-all">{linkTitle}</Link>
-            : null} 
-        </div>
+      <div className="flex justify-between items-center p-4">
+        <Typography className="text-2xl font-bold text-color-primary">
+          {title}
+        </Typography>
+        {linkHref && linkTitle ? (
+          <Link
+            href={linkHref}
+            className="md:text-lg text-md underline text-color-primary hover:text-color-accent transition-all"
+          >
+            {linkTitle}
+          </Link>
+        ) : null}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
