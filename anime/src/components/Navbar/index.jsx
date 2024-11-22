c4ce13f2e5e45327070d3011e6fd38fc8ba3b02e
Nabilfadilah <mohammadfadilah00@gmail.com>
2024-11-22 16:27:53 +0700

    Menambahkan componen userAction pada navbar

--- anime/src/components/Navbar/index.jsx
+++ anime/src/components/Navbar/index.jsx
@@ -1,16 +1,20 @@
-import InputSeacrh from '@/elements/input/InputSearch'
-import Link from 'next/link'
-import React from 'react'
+import InputSeacrh from "@/elements/input/InputSearch";
+import Link from "next/link";
+import React from "react";
+import UserAction from "./UserAction";
 
 const Navbar = () => {
-    return (
-        <header className='bg-color-accent'>
-            <div className='flex md:flex-row flex-col justify-between md:items-center p-4 gap-2'>
-                <Link href="/" className='font-bold text-white text-2xl'>ANIMEGS</Link>
-                <InputSeacrh placeholder="Cari anime..." />
-            </div>
-        </header>
-    )
-}
+  return (
+    <header className="bg-color-accent">
+      <div className="flex md:flex-row flex-col justify-between md:items-center p-4 gap-2">
+        <Link href="/" className="font-bold text-white text-2xl">
+          ANIMEGS
+        </Link>
+        <InputSeacrh placeholder="Cari anime..." />
+        <UserAction />
+      </div>
+    </header>
+  );
+};
 
-export default Navbar
+export default Navbar;
