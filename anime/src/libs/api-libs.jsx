003d965333af3a4460b03a921fc372273f75389e
Nabilfadilah <mohammadfadilah00@gmail.com>
2024-11-22 12:47:03 +0700

    Menambah fecthing api rekomendasi

--- anime/src/libs/api-libs.jsx
+++ anime/src/libs/api-libs.jsx
@@ -1,8 +1,16 @@
-// fetching api 
-export const getAnimeResponse = async(resource, query) => {
-    // server component, gak bisa pake useEffect, useState, dll..
-    const response = await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/${resource}?${query}`)
-    // keluarkan dulu datanya
-    const anime = await response.json()
-    return anime
-}
+// fetching api
+export const getAnimeResponse = async (resource, query) => {
+  // server component, gak bisa pake useEffect, useState, dll..
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_API_BASE_URL}/${resource}?${query}`
+  );
+  // keluarkan dulu datanya
+  const anime = await response.json();
+  return anime;
+};
+
+// double map by recomendasi
+export const getNestedAnimeResponse = async (resource, objectProperty) => {
+  const response = await getAnimeResponse(resource);
+  return response.data.flatMap((item) => item.entry);
+};
