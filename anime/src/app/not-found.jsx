33066c637c048990bfb8c50ee5ce0a73db67fe61
Nabilfadilah <mohammadfadilah00@gmail.com>
2024-11-23 06:40:27 +0700

    Update menambahkan useRouter untuk button back not found

--- anime/src/app/not-found.jsx
+++ anime/src/app/not-found.jsx
@@ -1,19 +1,29 @@
-"use client"
+"use client";
 
 import { LuFileSearch } from "react-icons/lu";
-import Link from "next/link";
 import Typography from "@/elements/button/text/Typography";
+import Button from "@/elements/button/Button";
+import { useRouter } from "next/navigation";
 
 const Page = () => {
-  return (
-    <div className='min-h-screen max-w-xl mx-auto flex justify-center items-center'>
-        <div className="flex justify-center items-center gap-4 flex-col">
-            <LuFileSearch size={44} className="text-color-accent" />
-            <Typography className="text-color-accent text-4xl font-bold">NOT FOUND</Typography>
-            <Link href="/" className="text-color-primary hover:text-color-accent transition-all underline">Kembali</Link>
-        </div>
-    </div>
-  )
-}
+  const router = useRouter();
 
-export default Page
+  return (
+    <div className="min-h-screen max-w-xl mx-auto flex justify-center items-center">
+      <div className="flex justify-center items-center gap-4 flex-col">
+        <LuFileSearch size={44} className="text-color-accent" />
+        <Typography className="text-color-accent text-4xl font-bold">
+          NOT FOUND
+        </Typography>
+        <Button
+          onClick={() => router.back()}
+          className="text-color-primary hover:text-color-accent transition-all underline"
+        >
+          Kembali
+        </Button>
+      </div>
+    </div>
+  );
+};
+
+export default Page;
