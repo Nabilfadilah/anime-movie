6dbfa920f1c302447fac7634892a693fea746d15
Nabilfadilah <mohammadfadilah00@gmail.com>
2024-11-22 08:31:21 +0700

    Menambah fungsi fetch api anime dan memanggil component

--- anime/src/app/populer/page.jsx
+++ anime/src/app/populer/page.jsx
@@ -1,9 +1,45 @@
-import React from 'react'
+"use client"; // Menjadikan ini Client Component
 
-const page = () => {
-    return (
-        <div>haha</div>
-    )
-}
+import AnimeList from "@/components/AnimeList";
+import HeaderMenu from "@/components/utilities/HeaderMenu";
+import Pagination from "@/components/utilities/Pagination";
+import React, { useEffect, useState } from "react";
 
-export default page
+const Page = () => {
+  const [page, setPage] = useState(1);
+  const [topAnime, setTopAnime] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setLoading(true);
+      try {
+        const response = await fetch(
+          `${process.env.NEXT_PUBLIC_API_BASE_URL}/top/anime?page=${page}`
+        );
+        const data = await response.json();
+        setTopAnime(data);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, [page]);
+
+  return (
+    <>
+      <HeaderMenu title={`ANIME TERPOPULER #${page}`} />
+      {/* <div>{loading ? "Loading..." : "FETCHING ALL DATA"}</div> */}
+      <AnimeList api={topAnime} />
+      <Pagination
+        page={page}
+        lastPage={topAnime.pagination?.last_visible_page}
+        setPage={setPage}
+      />
+    </>
+  );
+};
+export default Page;
