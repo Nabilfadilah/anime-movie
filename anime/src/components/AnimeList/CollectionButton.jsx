709f8020c502e804199bf76b48a33d897faac582
Nabilfadilah <mohammadfadilah00@gmail.com>
2024-11-24 14:43:00 +0700

    Membuat component button collection untuk menambahkan anime ke my collection

---
+++ anime/src/components/AnimeList/CollectionButton.jsx
@@ -0,0 +1,46 @@
+"use client";
+
+import Button from "@/elements/button/Button";
+import React, { useState } from "react";
+
+const CollectionButton = ({ anime_mal_id, user_email }) => {
+  const [isCreated, setIsCreated] = useState(false);
+
+  const handleCollection = async (event) => {
+    event.preventDefault();
+
+    const data = { anime_mal_id, user_email };
+    const response = await fetch("/api/v1/collection", {
+      method: "POST",
+      body: JSON.stringify(data),
+    });
+
+    if (!response.ok) {
+      console.error("Failed to create collection:", response.statusText);
+      return;
+    }
+
+    const collection = await response.json();
+    if (collection.isCreated) {
+      setIsCreated(true);
+    }
+    return;
+  };
+
+  return (
+    <>
+      {isCreated ? (
+        <p className="text-color-primary">Berhasil ditambahkan</p>
+      ) : (
+        <Button
+          onClick={handleCollection}
+          className="px-2 py-1 mt-2 bg-color-primary hover:bg-color-accent"
+        >
+          Add To Collection
+        </Button>
+      )}
+    </>
+  );
+};
+
+export default CollectionButton;
