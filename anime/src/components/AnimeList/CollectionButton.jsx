998607b5b4469b04caef762cfb25bacedffc936e
Nabilfadilah <mohammadfadilah00@gmail.com>
2024-12-06 20:24:06 +0700

    Menambah parameter anime_image dan anime_title untuk collectionButton

--- anime/src/components/AnimeList/CollectionButton.jsx
+++ anime/src/components/AnimeList/CollectionButton.jsx
@@ -3,13 +3,19 @@
 import Button from "@/elements/button/Button";
 import React, { useState } from "react";
 
-const CollectionButton = ({ anime_mal_id, user_email }) => {
+const CollectionButton = ({
+  anime_mal_id,
+  user_email,
+  anime_image,
+  anime_title,
+}) => {
   const [isCreated, setIsCreated] = useState(false);
 
   const handleCollection = async (event) => {
     event.preventDefault();
 
-    const data = { anime_mal_id, user_email };
+    // kalau punya data baru ya tinggal tambah parameter
+    const data = { anime_mal_id, user_email, anime_image, anime_title };
     const response = await fetch("/api/v1/collection", {
       method: "POST",
       body: JSON.stringify(data),
