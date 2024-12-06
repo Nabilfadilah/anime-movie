7b7484da2abc6c24c05d89ced83b45eebb6fadb3
Nabilfadilah <mohammadfadilah00@gmail.com>
2024-12-06 23:50:07 +0700

    Menjalankan fungsi api comment input

--- anime/src/components/AnimeList/CommentInput.jsx
+++ anime/src/components/AnimeList/CommentInput.jsx
@@ -2,25 +2,53 @@
 
 import Button from "@/elements/button/Button";
 import Typography from "@/elements/button/text/Typography";
+import { useRouter } from "next/navigation";
 import React, { useState } from "react";
 
-const CommentInput = () => {
+const CommentInput = ({ anime_mal_id, user_email, username, anime_title }) => {
   const [comment, setComment] = useState("");
+  const [isCreated, setIsCreated] = useState(false);
+
+  // agar langsung muncul ketika udah posting komentar
+  const router = useRouter();
 
   const handleInput = (event) => {
     setComment(event.target.value);
   };
 
-  const handlePosting = (event) => {
+  const handlePosting = async (event) => {
     event.preventDefault();
-    console.log(comment);
+
+    // kalau punya data baru ya tinggal tambah parameter
+    const data = { anime_mal_id, user_email, comment, username, anime_title };
+    const response = await fetch("/api/v1/comment", {
+      method: "POST",
+      body: JSON.stringify(data),
+    });
+
+    if (!response.ok) {
+      console.error("Failed to create comment:", response.statusText);
+      return;
+    }
+
+    const postComment = await response.json();
+    if (postComment.isCreated) {
+      setIsCreated(true);
+      setComment(""); // agar clear textera
+      router.refresh(); // update semua state
+    }
+    return;
   };
 
   return (
-    <div className="mt-4 flex flex-col gap-2">
+    <div className="flex flex-col gap-2">
+      {isCreated && (
+        <p className="text-color-accent">postingan komentar terkirim...</p>
+      )}
       <Typography className="text-color-primary">
         <textarea
           onChange={handleInput}
+          value={comment}
           className="w-full h-28 text-color-dark rounded-lg p-2"
         ></textarea>
         <Button
