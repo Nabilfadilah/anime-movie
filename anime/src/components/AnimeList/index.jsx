7742dda65c752b80f2284eee6bd025c5b36e1314
Nabilfadilah <mohammadfadilah00@gmail.com>
2025-01-13 21:25:05 +0700

    Merubah tampilan card anime list

--- anime/src/components/AnimeList/index.jsx
+++ anime/src/components/AnimeList/index.jsx
@@ -3,29 +3,30 @@ import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 
-const AnimeList = ({ api }) => {
+const AnimeList = ({api}) => {
   return (
-    <div className="grid md:grid-cols-4 sm:grid-cols-3 grid-cols-2 gap-4 px-4">
-      {api.data?.map((anime, index) => {
-        return (
-          <Link
-            key={index}
-            href={`/anime/${anime.mal_id}`}
-            className="cursor-pointer text-color-primary hover:text-color-accent transition-all"
-          >
-            <Image
-              src={anime.images.webp.image_url}
-              alt="..."
-              width={350}
-              height={350}
-              className="w-full max-h-64 object-cover rounded-lg"
-            />
-            <Typography className="font-bold md:text-xl text-md p-3">
-              {anime.title}
-            </Typography>
-          </Link>
-        );
-      })}
+    <div className="grid md:grid-cols-4 sm:grid-cols-3 grid-cols-2 gap-6 px-4">
+      {api.data?.map((anime, index) => (
+        <Link
+          key={index}
+          href={`/anime/${anime.mal_id}`}
+          className="group bg-white rounded-lg shadow-xl shadow-gray-500 overflow-hidden transition-transform transform hover:scale-105"
+        >
+          {/* Gambar Anime */}
+          <Image
+            src={anime.images.webp.image_url}
+            alt={anime.title}
+            width={350}
+            height={350}
+            className="w-full h-56 object-cover group-hover:brightness-90 transition-all"
+          />
+
+          {/* Judul Anime */}
+          <div className="p-4 bg-gray-200 text-black">
+            <h3 className="font-bold text-lg truncate">{anime.title}</h3>
+          </div>
+        </Link>
+      ))}
     </div>
   );
 };
