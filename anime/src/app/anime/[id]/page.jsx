4472d817bb5b9ccee279dc664bde6d37ad8d7080
Nabilfadilah <mohammadfadilah00@gmail.com>
2025-01-13 21:57:34 +0700

    Merubah banyak tampilan untuk detail anime, mulai dari posisi component dan desain masing-masing component

--- anime/src/app/anime/[id]/page.jsx
+++ anime/src/app/anime/[id]/page.jsx
@@ -1,15 +1,15 @@
-import { getAnimeResponse } from "@/libs/api-libs";
+import {getAnimeResponse} from "@/libs/api-libs";
 import VideoPlayer from "@/components/utilities/VideoPlayer";
 import Typography from "@/elements/button/text/Typography";
 import Image from "next/image";
 import React from "react";
 import CollectionButton from "@/components/AnimeList/CollectionButton";
-import { authUserSession } from "@/libs/auth-libs";
+import {authUserSession} from "@/libs/auth-libs";
 import prisma from "@/libs/prisma";
 import CommentInput from "@/components/AnimeList/CommentInput";
 import CommentBox from "@/components/AnimeList/CommentBox";
 
-const Page = async ({ params: { id } }) => {
+const Page = async ({params: {id}}) => {
   const anime = await getAnimeResponse(`anime/${id}`);
   console.log(anime);
 
@@ -17,101 +17,95 @@ const Page = async ({ params: { id } }) => {
 
   // menghilangkan button add colection jika sudah di tambahkan
   const collection = await prisma.collection.findFirst({
-    where: { user_email: user?.email, anime_mal_id: id },
+    where: {user_email: user?.email, anime_mal_id: id},
   });
   console.log(collection);
 
   return (
     <>
-      <div className="pt-4 px-4">
-        <Typography className="text-2xl text-color-primary">
-          {anime.data.title} - {anime.data.year}
-        </Typography>
-
-        {!collection && user && (
-          <CollectionButton
-            anime_mal_id={id}
-            user_email={user?.email}
-            anime_image={anime.data.images.webp.image_url}
-            anime_title={anime.data.title}
-          />
-        )}
-      </div>
-
-      <div className="pt-4 px-4 flex gap-2 text-color-primary overflow-x-auto">
-        <div className="w-36 flex flex-col justify-center items-center rounded border border-color-primary p-2">
-          <Typography>PERINGKAT</Typography>
-          <Typography>{anime.data.rank}</Typography>
+      <>
+        {/* Stats Section */}
+        <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-6 gap-4 p-6 bg-gray-100 text-gray-800">
+          {[
+            {label: "Peringkat", value: anime.data.rank},
+            {label: "Skor", value: anime.data.score},
+            {label: "Member", value: anime.data.members},
+            {label: "Favorit", value: anime.data.favorites},
+            {label: "Status", value: anime.data.status},
+            {label: "Durasi", value: anime.data.duration},
+          ].map((item, index) => (
+            <div
+              key={index}
+              className="flex flex-col items-center p-4 bg-white rounded-lg shadow-md shadow-gray-400"
+            >
+              <p className="text-sm font-semibold">{item.label}</p>
+              <p className="text-lg font-bold">{item.value}</p>
+            </div>
+          ))}
         </div>
-        <div className="w-36 flex flex-col justify-center items-center rounded border border-color-primary p-2">
-          <Typography>SKOR</Typography>
-          <Typography>{anime.data.score}</Typography>
+
+        {/* Header Section */}
+        <div className="relative bg-gradient-to-r from-gray-900 to-pink-950 text-white p-6">
+          <div className="flex flex-wrap items-center gap-6">
+            {/* Poster */}
+            <Image
+              src={anime.data.images.webp.image_url}
+              alt={anime.data.title}
+              width={300}
+              height={450}
+              className="rounded-lg shadow-lg"
+            />
+            {/* Title and Details */}
+            <div>
+              <h1 className="text-4xl font-bold">{anime.data.title}</h1>
+              <p className="text-lg mt-2">{anime.data.year}</p>
+              {!collection && user && (
+                <div className="mt-4 text-black">
+                  <CollectionButton
+                    anime_mal_id={id}
+                    user_email={user?.email}
+                    anime_image={anime.data.images.webp.image_url}
+                    anime_title={anime.data.title}
+                  />
+                </div>
+              )}
+            </div>
+          </div>
         </div>
-        {/* <div className="w-36 flex flex-col justify-center items-center rounded border border-color-primary p-2">
-          <Typography>RATING</Typography>
-          <Typography>{anime.data.rating}</Typography>
-        </div> */}
-        <div className="w-36 flex flex-col justify-center items-center rounded border border-color-primary p-2">
-          <Typography>MEMBER</Typography>
-          <Typography>{anime.data.members}</Typography>
+
+        {/* Synopsis Section */}
+        <div className="p-6 bg-white">
+          <h2 className="text-2xl font-bold text-gray-800 mb-4">Ringkasan</h2>
+          <p className="text-gray-700 text-justify">{anime.data.synopsis}</p>
         </div>
-        <div className="w-36 flex flex-col justify-center items-center rounded border border-color-primary p-2">
-          <Typography>FAVORIT</Typography>
-          <Typography>{anime.data.favorites}</Typography>
+
+        {/* Comments Section */}
+        <div className="p-6 bg-gradient-to-r from-gray-200 to-gray-300">
+          <h2 className="text-2xl font-bold text-gray-900 mb-4">
+            Komentar Penonton
+          </h2>
+          <CommentBox anime_mal_id={id} />
+          {user && (
+            <>
+              <h3 className="text-xl font-bold text-gray-900 mt-6">
+                Tambah Komentar
+              </h3>
+              <CommentInput
+                anime_mal_id={id}
+                user_email={user?.email}
+                username={user?.name}
+                anime_title={anime.data.title}
+              />
+            </>
+          )}
         </div>
-        <div className="w-36 flex flex-col justify-center items-center rounded border border-color-primary p-2">
-          <Typography>STATUS</Typography>
-          <Typography>{anime.data.status}</Typography>
+
+        {/* Trailer Section */}
+        <div className="p-6 bg-gray-100">
+          <h2 className="text-2xl font-bold text-gray-800 mb-4">Trailer</h2>
+          <VideoPlayer youtubeId={anime.data.trailer.youtube_id} />
         </div>
-        <div className="w-40 flex flex-col justify-center items-center rounded border border-color-primary p-2">
-          <Typography>DURASI</Typography>
-          <Typography>{anime.data.duration}</Typography>
-        </div>
-      </div>
-
-      <div className="pt-4 px-4 flex sm:flex-nowrap flex-wrap gap-2 text-color-primary">
-        <Image
-          src={anime.data.images.webp.image_url}
-          alt={anime.data.images.jpg.image_url}
-          width={250}
-          height={250}
-          className="w-full rounded object-cover"
-        />
-
-        <Typography className="text-justify text-xl">
-          RINGKASAN : {anime.data.synopsis}
-        </Typography>
-      </div>
-
-      {/* comment box */}
-      <div className="px-4 mt-2">
-        <Typography className="text-color-primary font-bold text-xl mb-1">
-          Komentar Penonton:
-        </Typography>
-        <CommentBox anime_mal_id={id} />
-      </div>
-
-      {/* input comment  */}
-      <div className="px-4">
-        {user && (
-          <Typography className="text-color-primary font-bold text-xl mb-1">
-            Tambah Komentar:
-          </Typography>
-        )}
-
-        {user && (
-          <CommentInput
-            anime_mal_id={id}
-            user_email={user?.email}
-            username={user?.name}
-            anime_title={anime.data.title}
-          />
-        )}
-      </div>
-
-      <div>
-        <VideoPlayer youtubeId={anime.data.trailer.youtube_id} />
-      </div>
+      </>
     </>
   );
 };
