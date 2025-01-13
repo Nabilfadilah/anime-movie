140e1f28272c7041634d0b4ccae1728965deede6
Nabilfadilah <mohammadfadilah00@gmail.com>
2025-01-13 21:02:51 +0700

    Merubah tampilan button dashboard dan logout serta menambah icon

--- anime/src/components/Navbar/UserAction.jsx
+++ anime/src/components/Navbar/UserAction.jsx
@@ -1,5 +1,7 @@
 import Link from "next/link";
-import { authUserSession } from "@/libs/auth-libs";
+import {authUserSession} from "@/libs/auth-libs";
+import {FaHome, FaSignOutAlt} from "react-icons/fa";
+import {MdDashboard} from "react-icons/md";
 
 const UserAction = async () => {
   const user = await authUserSession();
@@ -7,19 +9,21 @@ const UserAction = async () => {
   const actionURL = user ? "/api/auth/signout" : "/api/auth/signin";
 
   return (
-    <div className="flex gap-4 justify-between">
+    <div className="flex gap-4 items-center">
       {user ? (
         <Link
           href="/users/dashboard"
-          className="bg-color-secondary text-color-primary rounded-md py-1 px-8"
+          className="flex items-center bg-white text-gray-900 rounded-md py-2 px-6 hover:bg-opacity-80 transition-all gap-2"
         >
+          <FaHome size={20} /> {/* Ikon Dashboard */}
           Dashboard
         </Link>
       ) : null}
       <Link
         href={actionURL}
-        className="bg-color-dark text-color-primary rounded-md py-1 px-8"
+        className="flex items-center bg-pink-800 text-gray-200 rounded-md py-2 px-6 hover:bg-opacity-80 transition-all gap-2"
       >
+        <FaSignOutAlt size={20} /> {/* Ikon Logout */}
         {actionLabel}
       </Link>
     </div>
