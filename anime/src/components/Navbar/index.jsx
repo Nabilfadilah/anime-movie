e0c9dc24ba914147be719b140f52ebfce2a72e7c
Nabilfadilah <mohammadfadilah00@gmail.com>
2025-01-13 21:03:21 +0700

    Merubah tampilan navbar

--- anime/src/components/Navbar/index.jsx
+++ anime/src/components/Navbar/index.jsx
@@ -2,15 +2,28 @@ import InputSeacrh from "@/elements/input/InputSearch";
 import Link from "next/link";
 import React from "react";
 import UserAction from "./UserAction";
+import {BiSolidMoviePlay} from "react-icons/bi";
 
 const Navbar = () => {
   return (
-    <header className="bg-color-accent">
-      <div className="flex md:flex-row flex-col justify-between md:items-center p-4 gap-2">
-        <Link href="/" className="font-bold text-white text-2xl">
+    <header className="bg-orange-950 shadow-2xl">
+      <div className="flex md:flex-row flex-col justify-between items-center p-4 gap-4">
+        {/* Logo */}
+        <Link
+          href="/"
+          className="flex items-center font-bold text-white text-2xl hover:opacity-90 transition-opacity gap-2"
+        >
+          <BiSolidMoviePlay size={28} className="text-yellow-500" />{" "}
+          {/* Ikon Movie */}
           ANIMEGS
         </Link>
-        <InputSeacrh placeholder="Cari anime..." />
+
+        {/* Search Input */}
+        <div className="w-full md:w-auto flex-grow md:flex-grow-0">
+          <InputSeacrh placeholder="Cari anime..." />
+        </div>
+
+        {/* User Actions */}
         <UserAction />
       </div>
     </header>
