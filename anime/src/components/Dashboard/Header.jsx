c11cfeebf2cd715dad00fef4fbc01273500c0180
Nabilfadilah <mohammadfadilah00@gmail.com>
2025-01-14 21:27:50 +0700

    Merubah background color button back

--- anime/src/components/Dashboard/Header.jsx
+++ anime/src/components/Dashboard/Header.jsx
@@ -4,10 +4,10 @@ import Button from "@/elements/button/Button";
 import Typography from "@/elements/button/text/Typography";
 import Link from "next/link";
 import React from "react";
-import { IoMdArrowBack } from "react-icons/io";
-import { useRouter } from "next/navigation";
+import {IoMdArrowBack} from "react-icons/io";
+import {useRouter} from "next/navigation";
 
-const Header = ({ title }) => {
+const Header = ({title}) => {
   const router = useRouter();
 
   const handleBack = (event) => {
@@ -17,10 +17,12 @@ const Header = ({ title }) => {
 
   return (
     <div className="flex items-center justify-between mb-4">
-      <Typography className="text-2xl text-color-primary">{title}</Typography>
+      <Typography className="text-2xl text-black font-semibold">
+        {title}
+      </Typography>
 
       <Button
-        className="bg-color-primary hover:bg-color-accent"
+        className="bg-orange-950 hover:bg-orange-900 text-white p-2"
         onClick={handleBack}
       >
         <span className="flex items-center space-x-1">
